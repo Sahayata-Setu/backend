ca73617a505d8041e97c02a2e25e9ada722ee8f4
Tasnimul H. Tauhid <thtauhid.71@gmail.com>
2022-10-05 01:49:38 +0530

    Update multer.js

--- middleware/multer.js
+++ middleware/multer.js
@@ -2,12 +2,18 @@ const { S3Client } = require('@aws-sdk/client-s3')
 const multer = require('multer')
 const multerS3 = require('multer-s3')
 
-const s3 = new S3Client()
+const s3 = new S3Client({
+	region: process.env.REGION,
+	credentials: {
+		accessKeyId: process.env.ACCESS_KEY_ID,
+		secretAccessKey: process.env.SECRET_ACCESS_KEY,
+	},
+})
 
 const upload = multer({
 	storage: multerS3({
 		s3,
-		bucket: process.env.AWS_BUCKET_NAME,
+		bucket: process.env.BUCKET_NAME,
 		metadata: function (req, file, cb) {
 			cb(null, { fieldName: file.fieldname })
 		},
