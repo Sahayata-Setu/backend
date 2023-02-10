5c3572a582942ee57ca3fd5c3a91129272166827
Tasnimul H. Tauhid <thtauhid.71@gmail.com>
2023-02-11 01:22:40 +0530

    enum activated

--- models/donation.js
+++ models/donation.js
@@ -18,7 +18,7 @@ const donationSchema = new Schema(
 		},
 		category: {
 			type: String,
-			// enum: ['Food', 'Clothes', 'Book', 'Toys'],
+			enum: ['Food', 'Clothes', 'Book', 'Toys'],
 			required: true,
 		},
 		description: {
